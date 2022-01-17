a59304b76c5bfe4871752490e570b719eb340291
nrot <nrot13@gmail.com>
2022-01-17 18:28:34 +0300

    Add grayscale, image scale.

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,19 @@
 extern crate clap;
-use clap::{App, arg};
+use clap::{arg, App, Arg};
+use regex::Regex;
 
-pub fn new_app()-> App<'static>{
+pub fn new_app() -> App<'static> {
     App::new("").args(&[
-        arg!(<FILE> "file to show"),
-        arg!(--filter [filter] "filter type: Nearest,Triangle,CatmullRom,Gaussian,Lanczos3"),
-        arg!(-s --"scale_font" [scale_font] "scale of font to correct image"),
-    ])
+        arg!(<FILE> "File to show"),
+        arg!(--filter [filter] "Filter type: Nearest,Triangle,CatmullRom,Gaussian,Lanczos3. Default Triangle."),
+        arg!(-s --"scale_font" [scale_font] "Scale of font to correct image. Default value = 0.5"),
+        arg!(--scale [scale] "Custom scale"),
+        Arg::new("resolution")
+        .short('r')
+        .long("resolution")
+        .value_name("resolution")
+        .help("Set exact resolution. Ignore scale_font. Format 80x54 - hxw")
+        .validator_regex(Regex::new(r"\d+x\d+").unwrap().to_owned(), "Format 80x54 - hxw."),
+        arg!(-g --grayscale "Grayscale image")
+   ])
 }
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@ use std::io::Write;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use image::{imageops::FilterType, GenericImageView};
-use termcolor::{BufferWriter, Color, ColorSpec, WriteColor};
+use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 mod args;
 mod lib_console;
 
-fn worker(img: &DynamicImage, csize: lib_console::Size, ft: FilterType, sf: f32) {
+struct WorkerArgs<'a> {
+    img: &'a DynamicImage,
+    csize: lib_console::Size,
+    ft: FilterType,
+    sf: f32,
+    exact: bool,
+    grayscale: bool,
+    scale: f32,
+}
+
+fn worker(args: WorkerArgs) {
     let mut buffwr = BufferWriter::stdout(termcolor::ColorChoice::Always);
     let mut buff = buffwr.buffer();
-    let c = img.height() as f32 / (img.width() as f32);
-    let rimg = img
-        .resize_exact(
-            csize.cols as u32,
-            (csize.cols as f32 * c * sf).floor() as u32, //(csize.rows as f32 * sf).floor()
-            ft,
-        )
-        .into_rgb8();
+    let c = args.img.height() as f32 / (args.img.width() as f32);
+    let pmg = args.img.resize_exact(
+        (args.csize.cols as f32 * args.scale).floor() as u32,
+        ((if args.exact {
+            args.csize.rows as f32
+        } else {
+            (args.csize.cols as f32 * c * args.sf).floor()
+        }) * args.scale)
+            .floor() as u32,
+        args.ft,
+    );
+    let rimg = if args.grayscale {
+        pmg.grayscale().to_rgb8()
+    } else {
+        pmg.to_rgb8()
+    };
     println!("New image size h/w: {}/{}", rimg.height(), rimg.width());
     for y in 0..(rimg.height() - 1) {
         for x in 0..(rimg.width() - 1) {
@@ -64,25 +82,77 @@ fn main() {
         e => panic!("Unkown filter type: {}", e),
     };
 
-    let sf = arg
+    let mut sf = arg
         .value_of("scale_font")
-        .unwrap_or("0.75")
+        .unwrap_or("0.5")
         .parse::<f32>()
         .expect("scale_font must be float");
 
-    let csize = lib_console::get_terminal_size().expect("Run not in console?");
-    worker(&img, csize, ft, sf);
+    let mut csize = lib_console::get_terminal_size().expect("Run not in console?");
+    let mut scale = arg
+        .value_of("scale")
+        .unwrap_or("1.0")
+        .parse::<f32>()
+        .expect("Can`t parse scale");
+
+    let res = String::from(
+        arg.value_of("resolution")
+            .unwrap_or("")
+            .to_lowercase()
+            .trim(),
+    );
+    let mut exact = false;
+    if res != "" {
+        let rw = res.split("x").collect::<Vec<&str>>();
+        csize.rows = rw[0]
+            .parse::<u16>()
+            .expect("Can`t convert resolution to uint");
+        csize.cols = rw[1]
+            .parse::<u16>()
+            .expect("Can`t convert resolution to uint");
+        sf = 1.0;
+        exact = true;
+        scale = 1.0;
+    };
+
+    if scale > 1.0 {
+        let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+        let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)));
+        let _ = writeln!(&mut stdout, "Warning scale more then 1.0 can broke image");
+    };
+
+    let grayscale = arg.is_present("grayscale");
+
+    worker(WorkerArgs {
+        img: &img,
+        csize: csize,
+        ft: ft,
+        sf: sf,
+        exact: exact,
+        grayscale: grayscale,
+        scale: scale,
+    });
 }
 
-
 #[test]
-fn all_imgs(){
+fn all_imgs() {
     let csize = lib_console::get_terminal_size().expect("Run not in console?");
     println!("Terminal size h/w: {}/{}\n", &csize.rows, &csize.cols);
-    for file in std::fs::read_dir("imgs/").unwrap(){
+    for file in std::fs::read_dir("imgs/").unwrap() {
         let fp = file.unwrap().path();
         println!("Draw image: {:?}", fp);
-        let img = ImageReader::open(fp).expect("Can`t open file").decode().expect("Can`t decode image");
-        worker(&img, csize.clone(), FilterType::Triangle, 0.5);
+        let img = ImageReader::open(fp)
+            .expect("Can`t open file")
+            .decode()
+            .expect("Can`t decode image");
+        worker(WorkerArgs {
+            img: &img,
+            csize: csize.clone(),
+            ft: FilterType::Triangle,
+            sf: 0.5,
+            exact: false,
+            grayscale: false,
+            scale: 1.0,
+        });
     }
 }
